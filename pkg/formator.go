675fadb83c42ec69de6b6c4c7bdb22174b2ad38e
KunWuLuan <kunwuluan@gmail.com>
2022-11-22 20:24:05 +0800

    add properties

--- pkg/formator.go
+++ pkg/formator.go
@@ -1,12 +1,7 @@
 package treeformator
 
-import "fmt"
-
-const (
-	space   = "  "
-	last    = "\\-"
-	line    = "| "
-	notlast = "|-"
+import (
+	"fmt"
 )
 
 type TreeFormatorNodeInfo struct {
@@ -26,8 +21,19 @@ func NewTreeFormatorNodeInfo(r TreeFormator, root *TreeFormatorNodeInfo, isLast 
 	return &TreeFormatorNodeInfo{r, root, isLast}
 }
 
-func ShowTreeStruct(r TreeFormator) {
-	strs := dfs(NewTreeFormatorNodeInfo(r, nil, true))
+func ShowTreeStruct(r TreeFormator, opts ...Option) {
+	printer := &treePrinterImpl{
+		linewidth: 1,
+		space:     "  ",
+		last:      "\\-",
+		line:      "| ",
+		notlast:   "|-",
+	}
+	for _, opt := range opts {
+		opt(printer)
+	}
+
+	strs := printer.Dfs(NewTreeFormatorNodeInfo(r, nil, true))
 	for _, str := range strs {
 		fmt.Println(str)
 	}
@@ -37,7 +43,23 @@ func (r *TreeFormatorNodeInfo) Show() string {
 	return r.node.Show()
 }
 
-func buildPrefix(r *TreeFormatorNodeInfo) string {
+type Option func(TreePrinter) error
+
+type TreePrinter interface {
+	BuildPrefix(r *TreeFormatorNodeInfo) string
+	Dfs(r *TreeFormatorNodeInfo) (str []string)
+	SetProperty(name string, value interface{}) error
+}
+
+type treePrinterImpl struct {
+	linewidth int
+	space     string
+	last      string
+	line      string
+	notlast   string
+}
+
+func (tp *treePrinterImpl) BuildPrefix(r *TreeFormatorNodeInfo) string {
 	if r == nil {
 		return ""
 	}
@@ -46,18 +68,18 @@ func buildPrefix(r *TreeFormatorNodeInfo) string {
 		return cur
 	} else {
 		if r.isLast {
-			cur = last + cur
+			cur = tp.last + cur
 		} else {
-			cur = notlast + cur
+			cur = tp.notlast + cur
 		}
 	}
 	curNode := r.root
 	root := curNode.root
 	for root != nil {
 		if curNode.isLast {
-			cur = space + cur
+			cur = tp.space + cur
 		} else {
-			cur = line + cur
+			cur = tp.line + cur
 		}
 		curNode = root
 		root = curNode.root
@@ -65,14 +87,82 @@ func buildPrefix(r *TreeFormatorNodeInfo) string {
 	return cur
 }
 
-func dfs(r *TreeFormatorNodeInfo) (str []string) {
+func (tp *treePrinterImpl) Dfs(r *TreeFormatorNodeInfo) (str []string) {
 	if r == nil {
 		return []string{}
 	}
 	nextLevelNodes := r.NextLevel()
-	str = append(str, buildPrefix(r))
+	str = append(str, tp.BuildPrefix(r))
 	for i, child := range nextLevelNodes {
-		str = append(str, dfs(NewTreeFormatorNodeInfo(child, r, i == len(nextLevelNodes)-1))...)
+		str = append(str, tp.Dfs(NewTreeFormatorNodeInfo(child, r, i == len(nextLevelNodes)-1))...)
 	}
 	return
+}
+
+func (tp *treePrinterImpl) SetProperty(name string, value interface{}) error {
+	switch name {
+	case "linewidth":
+		l, ok := value.(int)
+		if !ok {
+			return fmt.Errorf("value of linewidth must be int")
+		}
+		tp.linewidth = l
+	case "line":
+		l, ok := value.(string)
+		if !ok {
+			return fmt.Errorf("value of line must be string")
+		}
+		tp.line = l
+	case "space":
+		l, ok := value.(string)
+		if !ok {
+			return fmt.Errorf("value of space must be string")
+		}
+		tp.space = l
+	case "notlast":
+		l, ok := value.(string)
+		if !ok {
+			return fmt.Errorf("value of notlast must be string")
+		}
+		tp.notlast = l
+	case "last":
+		l, ok := value.(string)
+		if !ok {
+			return fmt.Errorf("value of last must be string")
+		}
+		tp.last = l
+	default:
+		return fmt.Errorf("undefined property")
+	}
+	return nil
+}
+
+func WithLinewidth(l int) Option {
+	return func(tp TreePrinter) error {
+		return tp.SetProperty("linewidth", l)
+	}
+}
+
+func WithSpace(s string) Option {
+	return func(tp TreePrinter) error {
+		return tp.SetProperty("space", s)
+	}
+}
+
+func WithLine(s string) Option {
+	return func(tp TreePrinter) error {
+		return tp.SetProperty("line", s)
+	}
+}
+
+func WithLast(s string) Option {
+	return func(tp TreePrinter) error {
+		return tp.SetProperty("last", s)
+	}
+}
+
+func WithNoLast(s string) Option {
+	return func(tp TreePrinter) error {
+		return tp.SetProperty("nolast", s)
+	}
 }
